f7fd0e44caf7426500f98581915c5e2b9ba85c57
Bartek Mikorski <b.mikorski@gmail.com>
2024-11-15 13:17:15 +0900

    added delete to lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
+  # delete "lists/:id", to: "lists#destroy", as: :list
   resources :bookmarks, only: :destroy
 end
